8d617965a9e38bd9eed5debd9ca416b189833cdb
unknown <nwinant@lapetus.cyc.local>
2015-03-11 00:20:56 -0500

    Add basic configuration and function chaining

--- js/human-timer.js
+++ js/human-timer.js
@@ -1,8 +1,41 @@
 /**
  * HumanTimer base class
  */
-function HumanTimer(options) {
+function HumanTimer(opts) {
+    var config={
+      duration:0,
+      log: true
+    };
+    var log;
     
+    var updateLog = function() {
+      if (config.log) {
+        log = function(msg) { console.log(arguments); } 
+      } else {
+        log = function(msg) {};
+      }
+    }
     
-    console.log("HumanTimerJS instantiated!");
+    function my() {
+    };
+    
+    my.options = function(values) {
+      if (!arguments.length) return config;
+      for (var key in values) {
+        if (values.hasOwnProperty(key)) config[key]=values[key];
+      }
+      updateLog();
+      return my;
+    };
+    
+    my.options(opts);
+    log("HumanTimerJS instantiated!", config);
+    
+    my.duration = function(value) {
+      if (!arguments.length) return config.duration;
+      config.duration=value + config.duration;
+      return my;
+    };
+    
+    return my;
 }
