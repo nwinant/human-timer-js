96fb9d86494dff0d7aebf4f4b1954c0ca47418d2
Nathan Winant <nw@exegetic.net>
2015-03-12 10:50:36 -0500

    Slightly clarifies console output

--- js/human-timer.js
+++ js/human-timer.js
@@ -3,38 +3,38 @@
  */
 function HumanTimer(opts) {
     var config={
-      duration:0,
-      log: true
+        duration:0,
+        log: true
     };
     var log;
     
     var updateLog = function() {
-      if (config.log) {
-        log = function(msg) { console.log(arguments); } 
-      } else {
-        log = function(msg) {};
-      }
+        if (config.log) {
+            log = function(msg) { console.log(arguments); } 
+        } else {
+            log = function(msg) {};
+        }
     }
     
     function my() {
     };
     
     my.options = function(values) {
-      if (!arguments.length) return config;
-      for (var key in values) {
-        if (values.hasOwnProperty(key)) config[key]=values[key];
-      }
-      updateLog();
-      return my;
+        if (!arguments.length) return config;
+        for (var key in values) {
+            if (values.hasOwnProperty(key)) config[key]=values[key];
+        }
+        updateLog();
+        return my;
     };
     
     my.options(opts);
     log("HumanTimerJS instantiated!", config);
     
     my.duration = function(value) {
-      if (!arguments.length) return config.duration;
-      config.duration=value + config.duration;
-      return my;
+        if (!arguments.length) return config.duration;
+        config.duration=value;
+        return my;
     };
     
     return my;
