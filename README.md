bc48c330fd61d787dea549e24110d795847fa6b1
Nathan Winant <nw@exegetic.net>
2015-03-30 22:00:06 -0500

    Adds Jasmine-based ConfigurationSpec.js test

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-human-timer-js
-==============
+HumanTimerJS
+============
 
 A simple timer for human-scale user events, written in vanilla JS.
+
+
+Testing
+-------
+Tests are currently written in [Jasmine](http://jasmine.github.io/).
+There are [several ways to install Jasmine](https://github.com/jasmine/jasmine#installation),
+but to get started quickly from within the `human-timer-js` project:
+
+    mkdir jasmine
+    cd jasmine
+    git clone https://github.com/jasmine/jasmine.git
+    mv jasmine/dist/jasmine-standalone-2.2.0.zip .
+    unzip jasmine-standalone-2.2.0.zip
+
+It's up to you whether to leave the jasmine git repo lying around inside the jasmine directory.
+
+You can then open [SpecRunner.html](SpecRunner.html) in your browser.
---
+++ spec/ConfigurationSpec.js
@@ -0,0 +1,54 @@
+describe("Configuration", function() {
+
+    it("should have sane defaults", function() {
+        var timer = HumanTimer();
+        expect(timer.duration()).toEqual(0);
+        expect(timer.options().log).toEqual(true);
+        expect(timer.options()).toEqual({
+          "duration": 0,
+          "log": true
+        });
+    });
+
+    it("should not override defaults if given empty options", function() {
+      var timerNoOptions = HumanTimer();
+      var timerEmptyOptions = HumanTimer({});
+      expect(timerEmptyOptions.options()).toEqual(timerNoOptions.options());
+    });
+
+    it("should persist configuration options", function() {
+        var options = {
+            duration: 1000
+        };
+        var timer = HumanTimer(options);
+        expect(timer.duration()).toEqual(options.duration);
+        expect(timer.options().duration).toEqual(options.duration);
+        expect(timer.options().log).toEqual(true);
+        expect(timer.options()).toEqual({
+          "duration": 1000,
+          "log": true
+        });
+    });
+
+    it("should chain configuration setters", function() {
+        var timer = HumanTimer();
+        expect(timer.duration(50)).toEqual(timer);
+    });
+
+    it("no-arg config functions should return values", function() {
+        var timer = HumanTimer();
+        expect(timer.duration()).toEqual(0);
+        timer.duration(50);
+        expect(timer.duration()).toEqual(50);
+    });
+
+    it("should chain configuration setters & getters", function() {
+        var timer = HumanTimer();
+        expect(timer.duration(50)).toEqual(timer);
+        timer.duration(50);
+        expect(timer.duration()).toEqual(50);
+        expect(timer.duration(7000).duration()).toEqual(7000);
+        expect(timer.duration(500).duration()).toEqual(500);
+    });
+
+});
