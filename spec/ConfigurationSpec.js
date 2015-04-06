1adb9926eb0271b5a09279d52f03f2d349e9295c
unknown <nwinant@lapetus.cyc.local>
2015-04-05 21:25:41 -0500

    Adds test for starting when misconfiguration. Other test expectations a little more specific.

--- spec/ConfigurationSpec.js
+++ spec/ConfigurationSpec.js
@@ -2,8 +2,8 @@ describe("Configuration", function() {
 
     it("should have sane defaults", function() {
         var timer = HumanTimer.create();
-        expect(timer.duration()).toEqual(0);
-        expect(timer.options().log).toEqual(false);
+        expect(timer.duration()).toBe(0);
+        expect(timer.options().log).toBe(false);
         expect(timer.options()).toEqual(HumanTimer.defaults);
     });
 
@@ -20,7 +20,7 @@ describe("Configuration", function() {
         var timer = HumanTimer.create(options);
         expect(timer.duration()).toEqual(options.duration);
         expect(timer.options().duration).toEqual(options.duration);
-        expect(timer.options().log).toEqual(false);
+        expect(timer.options().log).toBe(false);
         expect(timer.options()).toEqual({
           "duration": 1000,
           "log": false
@@ -29,23 +29,27 @@ describe("Configuration", function() {
 
     it("should chain configuration setters", function() {
         var timer = HumanTimer.create();
-        expect(timer.duration(50)).toEqual(timer);
+        expect(timer.duration(50)).toBe(timer);
     });
 
     it("no-arg config functions should return values", function() {
         var timer = HumanTimer.create();
-        expect(timer.duration()).toEqual(0);
+        expect(timer.duration()).toBe(0);
         timer.duration(50);
-        expect(timer.duration()).toEqual(50);
+        expect(timer.duration()).toBe(50);
     });
 
     it("should chain configuration setters & getters", function() {
         var timer = HumanTimer.create();
-        expect(timer.duration(50)).toEqual(timer);
+        expect(timer.duration(50)).toBe(timer);
         timer.duration(50);
-        expect(timer.duration()).toEqual(50);
-        expect(timer.duration(7000).duration()).toEqual(7000);
-        expect(timer.duration(500).duration()).toEqual(500);
+        expect(timer.duration()).toBe(50);
+        expect(timer.duration(7000).duration()).toBe(7000);
+        expect(timer.duration(500).duration()).toBe(500);
+    });
+
+    it("should throw an exception if misconfigured", function() {
+      expect(HumanTimer.create().start).toThrow("No duration set.");
     });
 
 });
